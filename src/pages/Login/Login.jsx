a4eeea52a90c3a2e1d2f0ddcd4dc4cfae3e0c629
Osamaa3mmar <osamaammarothman@gmail.com>
2025-03-28 15:13:23 +0200

    some adding

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,8 @@
+import { Link } from "react-router-dom";
 export default function Login() {
   return (
     <div>
-      Login
+      <Link  to="/main" style={{color:"white"}}>main</Link>
     </div>
   )
 }
